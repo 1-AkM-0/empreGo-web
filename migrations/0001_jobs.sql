5ac6f842ef0bd35b8bf811a403299137f500e230
Pedro Yan <thepe880@gmail.com>
2026-02-17 22:45:15 -0300

    refactor: simplifica job status e remove dependências não usadas
    - Substitui a tabela "status" por um CHECK na tabala jobs
    - Adiciona a coluna updated_at na tabela jobs

--- migrations/0001_jobs.sql
+++ migrations/0001_jobs.sql
@@ -1,20 +1,14 @@
 -- +goose Up
 -- +goose StatementBegin
 
-CREATE TABLE IF NOT EXISTS status (
-    id INTEGER PRIMARY KEY,
-    status_name TEXT NOT NULL
-);
-
-INSERT INTO status (id, status_name) VALUES (1, "Nova"), (2, "Aplicada"), (3, "Entrevista"), (4, "Aprovado"), (5, "Rejeitado");
-
 CREATE TABLE IF NOT EXISTS jobs (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		title TEXT NOT NULL,
-		link TEXT UNIQUE,
-    status_id INTEGER REFERENCES status(id),
+		link TEXT UNIQUE NOT NULL,
+    status TEXT DEFAULT "new" CHECK (status IN ("new", "applied", "interview", "approved", "rejected")),
     source TEXT NOT NULL,
-    created_at TEXT DEFAULT CURRENT_TIMESTAMP
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    updated_at TEXT DEFAULT CURRENT_TIMESTAMP
 );
 
 -- +goose StatementEnd
@@ -22,5 +16,4 @@ CREATE TABLE IF NOT EXISTS jobs (
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE jobs;
-DROP TABLE status;
 -- +goose StatementEnd
