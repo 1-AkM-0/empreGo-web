ba43b1864917744bde137c26ddaf069868da729c
Pedro Yan <thepe880@gmail.com>
2026-02-20 19:46:49 -0300

    fix: remove campos desnecessários na tabela users
    - discord_id e name foram removidos pois não serão utilizados

--- migrations/0002_users.sql
+++ migrations/0002_users.sql
@@ -3,9 +3,7 @@
 
 CREATE TABLE IF NOT EXISTS users (
 		id TEXT PRIMARY KEY,
-		name TEXT NOT NULL,
 	  email TEXT UNIQUE NOT NULL,
-    discord_id INT,
     hash_password TEXT,
     created_at TEXT DEFAULT CURRENT_TIMESTAMP,
     updated_at TEXT DEFAULT CURRENT_TIMESTAMP
