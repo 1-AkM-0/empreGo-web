04715edfe0f0461e91af1e20a48c606ecce05f65
Pedro Yan <thepe880@gmail.com>
2026-02-20 19:54:57 -0300

    feat: define as rotas de autenticação e de applications

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,6 +12,20 @@ func (app *application) routes() *gin.Engine {
 			jobs.GET("/", app.getAllJobsHandler)
 			jobs.GET("/:id", app.getJobByIDHandler)
 		}
+		auth := v1.Group("/auth")
+		{
+			auth.POST("/register")
+			auth.POST("/login")
+		}
+		application := v1.Group("/application")
+		{
+			application.GET("/")
+			application.GET("/:id")
+			application.POST("/")
+			application.PATCH("/:id")
+			application.DELETE("/:id")
+
+		}
 	}
 	return router
 
